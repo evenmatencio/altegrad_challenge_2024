3e7aabec15b4d479deb5be61fa13d0f5105c6943
JoyRoseDdS <joy-rose.dunoyer-de-segonzac@eleves.enpc.fr>
2024-01-11 12:18:20 +0100

    scibert use for text encoder + formatting

--- Model.py
+++ Model.py
@@ -32,30 +32,30 @@ class GraphEncoder(nn.Module):
         x = self.mol_hidden1(x).relu()
         x = self.mol_hidden2(x)
         return x
-    
+
 class TextEncoder(nn.Module):
     def __init__(self, model_name):
         super(TextEncoder, self).__init__()
         self.bert = AutoModel.from_pretrained(model_name)
-        
+
     def forward(self, input_ids, attention_mask):
         encoded_text = self.bert(input_ids, attention_mask=attention_mask)
         #print(encoded_text.last_hidden_state.size())
         return encoded_text.last_hidden_state[:,0,:]
-    
+
 class Model(nn.Module):
     def __init__(self, model_name, num_node_features, nout, nhid, graph_hidden_channels):
         super(Model, self).__init__()
         self.graph_encoder = GraphEncoder(num_node_features, nout, nhid, graph_hidden_channels)
         self.text_encoder = TextEncoder(model_name)
-        
+
     def forward(self, graph_batch, input_ids, attention_mask):
         graph_encoded = self.graph_encoder(graph_batch)
         text_encoded = self.text_encoder(input_ids, attention_mask)
         return graph_encoded, text_encoded
-    
+
     def get_text_encoder(self):
         return self.text_encoder
-    
+
     def get_graph_encoder(self):
         return self.graph_encoder
